e5dceaa97e1fbf1b243db5e46b9820b890bdd6f6
Matthew <matthewdb85@gmail.com>
2016-11-11 15:25:41 +0000

    Completing new school POST route

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\School;
+use App\School;
 
 class SchoolController extends Controller
 {
